9250c99d5767ddcb3c566ad904ae4011a5958f45
Muhammadqodir <muwicef@gmail.com>
2023-01-06 23:37:47 +0500

    Add Splash Page

--- lib/presentation/pages/splash/splash_page.dart
+++ lib/presentation/pages/splash/splash_page.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:haramayn/core/constants/app_assets.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarIconBrightness: Brightness.dark,
+      ),
+    );
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
         child: Center(
-          child: Text('Splash Page'),
+          child: Image.asset(
+            width: 156,
+            AppAssets.images.logo,
+          ),
         ),
       ),
     );
